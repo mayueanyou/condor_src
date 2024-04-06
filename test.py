c5ace218df54b83b875fa1905d2eaf878cba55f3
Yue Ma <mayueanyou@gmail.com>
2024-04-06 17:03:55 -0400

    update

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os,sys,argparse
+import torch
 
 def main(args):
+    print(torch.cuda.get_device_name(0))  if torch.cuda.is_available() else print('No GPU')
     print(f"hello {args.str}")
 
 if __name__ == '__main__':
