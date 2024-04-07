9cde3518c89b1ed9c62bf22a4ad550803a8d65f7
Yue Ma <mayueanyou@gmail.com>
2024-04-06 21:31:56 -0400

    update

--- submit.py
+++ submit.py
@@ -3,20 +3,20 @@ import os,sys,subprocess
 def create_folder(path):
     if not os.path.exists(path):os.makedirs(path)
 
-def get_sub_str(arguments):
+def get_sub_str(arguments,condor_version = 'base'):
     file_path=os.path.abspath(__file__)
     current_path =  os.path.abspath(os.path.dirname(file_path) + os.path.sep + ".")
     lines = [f'executable = {current_path}/conda_wrapper\n']
-    lines.append('arguments = %s\n'%arguments)
+    lines.append('arguments = %s %s\n'%(condor_version,arguments))
     lines.append('error= ./err\noutput= ./out\nlog= ./log\n')
     lines.append('Requirements = (TotalGPUs > 0)\n+request_gpus = 1\nqueue')
     return lines
 
-def condor_submit(path, py_file, py_arguments):
+def condor_submit(path, py_file, py_arguments, condor_version = 'py3.10'):
     create_folder(path)
     arguments = py_file + ' ' + py_arguments
     with open(path+"/sub", "w") as file:
-        file.writelines(get_sub_str(arguments))
+        file.writelines(get_sub_str(arguments,condor_version))
     subprocess.run(["rm log & condor_submit sub"], cwd=path, shell=True)
 
 def quick_start():
