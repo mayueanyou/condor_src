6f46372feedd304c30f8d7014e43efa3cc478c1b
Yue Ma <mayueanyou@gmail.com>
2024-08-23 17:43:50 -0400

    update

--- submit.py
+++ submit.py
@@ -19,12 +19,26 @@ def condor_submit(path, py_file, py_arguments, condor_version = 'base',extra_req
         file.writelines(get_sub_str(arguments,condor_version,path,extra_requirements))
     subprocess.run(["rm log & condor_submit sub"], cwd=path, shell=True)
 
+def submit_wrap_demo(folder,argument):
+    current_path =  os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
+    path = current_path + '/condor/' + folder
+    py_file = current_path + '/test.py'
+    py_arguments = argument
+    condor_version = 'base'
+    extra_requirements = ''
+    condor_submit(path = path, py_file = py_file, py_arguments=py_arguments, condor_version=condor_version,extra_requirements=extra_requirements)
+
 def quick_start():
     file_path=os.path.abspath(__file__)
     current_path =  os.path.abspath(os.path.dirname(file_path) + os.path.sep + ".")
     py_file = current_path + '/test.py'
-    condor_submit(current_path + '/condor/test_1', py_file, '-str world_1')
-    condor_submit(current_path + '/condor/test_2', py_file, '-str world_2')
+    condor_submit(current_path + '/condor/test_1', py_file, '-str world_1','py3.10')
+    condor_submit(current_path + '/condor/test_2', py_file, '-str world_2','py3.10')
+
+def quick_start_wrap():
+    submit_wrap_demo('test_1','-str world_1')
+    submit_wrap_demo('test_2','-str world_2')
 
 if __name__ == '__main__':
-    quick_start()
+    #quick_start()
+    quick_start_wrap()
