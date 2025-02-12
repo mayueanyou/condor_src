72aab5b27d18736abe7326c0f8f11ba246051fe5
Yue Ma <mayueanyou@gmail.com>
2025-02-12 15:45:01 -0500

    update

--- condor_controller.py
+++ condor_controller.py
@@ -1,18 +1,23 @@
 import os,sys,subprocess
 
 class CondorController:
-    def __init__(self,base_path:str,py_file:str,condor_version:str='base',extra_requirements:str='') -> None:
+    def __init__(self,base_path:str,py_file:str,condor_version:str='base',ignore_machine:list[str]=[],extra_requirements:str='') -> None:
         self.base_path = base_path
         self.py_file = py_file
         self.condor_version = condor_version
+        self.ignore_machine = ''.join([f'&&(Machine != "{machine}")' for machine in ignore_machine])
         self.extra_requirements = extra_requirements
         self.condor_script_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".") + '/conda_wrapper'
     
     def generate_sub_str(self,arguments,path):
         lines = [f'executable = {self.condor_script_path}\n',
                 f'arguments = {self.condor_version} {arguments}\n',
-                f'error= {path}/err\noutput= {path}/out\nlog= {path}/log\n',
-                f'Requirements = (TotalGPUs > 0){self.extra_requirements}\n+request_gpus = 1\nqueue']
+                f'error= {path}/err\n',
+                f'output= {path}/out\n',
+                f'log= {path}/log\n',
+                f'Requirements = (TotalGPUs > 0){self.ignore_machine}{self.extra_requirements}\n',
+                f'+request_gpus = 1\n',
+                f'queue']
         return lines
     
     def submit(self,sub_path, py_arguments):
